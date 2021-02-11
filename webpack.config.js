a9f9973d7228a9a45c798b447ccaa1ba440b6e5c
Christian Gonçalves <christiangoncalvesr@gmail.com>
2021-02-11 00:32:30 -0300

    build: fixed build command

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,56 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+// eslint-disable-next-line func-names
+module.exports = function (webpackEnv) {
+  const isEnvDevelopment = webpackEnv === 'development';
+
+  return {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+      path: isEnvDevelopment
+        ? path.resolve(__dirname, 'public')
+        : path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'public'),
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-      },
-      {
-        test: /.*\.(gif|svg|png|jpe?g)$/i,
-        use: {
-          loader: 'file-loader',
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
         },
-      },
-      {
-        test: /\.(graphql|gql)$/,
-        exclude: /node_modules/,
-        loader: 'graphql-tag/loader',
-      },
+        {
+          test: /.*\.(gif|svg|png|jpe?g)$/i,
+          use: {
+            loader: 'file-loader',
+          },
+        },
+        {
+          test: /\.(graphql|gql)$/,
+          exclude: /node_modules/,
+          loader: 'graphql-tag/loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+  };
 };
